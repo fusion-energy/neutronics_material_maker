97540ab8c51ecfa3d46448d544954c2feb0d9197
Jon <mail@jshimwell.com>
2018-07-02 18:07:49 +0100

    added matplotlib

--- tests/testsuite.py
+++ tests/testsuite.py
@@ -22,8 +22,6 @@ from tests.module_tests import Compound_tests
 from tests.module_tests import Homogenised_mixture_tests
 from tests.module_tests import Example_materials_tests
 
-from neutronics_material_maker.refmaterials import * 
-
 def main():
     unittest.TextTestRunner(verbosity=3).run(unittest.TestSuite())
 
