e183eeee79ad10d2ffc795e77e8734cd81ea5a26
Jon <mail@jshimwell.com>
2018-07-04 11:11:43 +0100

    now using refmaterials tests from refmaterials

--- tests/testsuite.py
+++ tests/testsuite.py
@@ -21,9 +21,11 @@ from tests.module_tests import Material_tests
 from tests.module_tests import Compound_tests
 from tests.module_tests import Homogenised_mixture_tests
 from tests.module_tests import Example_materials_tests
-from tests.ref_material_tests import test_property
-from tests.ref_material_tests import test_materials
-from tests.ref_material_tests import test_liquids
+
+from neutronics_material_maker.refmaterials import test_property
+from neutronics_material_maker.refmaterials import test_materials
+from neutronics_material_maker.refmaterials import test_liquids
+
 
 def main():
     unittest.TextTestRunner(verbosity=3).run(unittest.TestSuite())
