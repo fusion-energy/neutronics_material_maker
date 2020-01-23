4cff886d213fca8471f7356e171d3945a1f1a1a8
shimwell <shimwell@yahoo.com>
2020-01-23 10:37:31 +0000

    removed paramak from test

--- tests/test_Multmaterial.py
+++ tests/test_Multmaterial.py
@@ -27,49 +27,16 @@ ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
 import pytest
 import unittest
 
-from paramak import Shape
+from neutronics_material_maker import MultiMaterial
 
 # test command
 # pytest tests -v --cov=paramak --cov-report term --cov-report html:htmlcov --cov-report xml --junitxml=test-reports/junit.xml
 # from head paramak directory
 
-class test_object_properties(unittest.TestCase):
+class test_mixed_material(unittest.TestCase):
 
-        def test_shape_default_properties(self):
-
-                test_shape = Shape()
-
-                assert test_shape.points == None
-
-        def test_correct_points(self):
-
-                test_shape = Shape()
-
-                test_shape.points = [
-                        (0, 0, 200),
-                        (200, 0, 100),
-                        (0, 0, 0),
-                        (0, 0, 200)
-                ]
-
-                assert test_shape.points[0] == test_shape.points[-1]
-
-
-        def test_incorrect_points(self):
-
-                test_shape = Shape()
-
-                def incorrect_points_function():
-
-                        test_shape.points = [
-                                (0, 0, 200),
-                                (200, 0, 100),
-                                (0, 0, 0),
-                                (0, 0, 50)
-                        ]
-
-                self.assertRaises(ValueError, incorrect_points_function)
-
+        def density_from_mixed_material(self):
+                pass
 
 if __name__ == '__main__':
         unittest.main()
