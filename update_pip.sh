fc6a03a4b4e6199633fbe1e01f9d6f8bf5951dfd
shimwell <shimwell@yahoo.com>
2020-01-27 13:55:26 +0000

    added cleaning dir from old dist

--- update_pip.sh
+++ update_pip.sh
@@ -1,8 +1,19 @@
 
+#install required packages
 pip3 install bump
 pip3 install setuptools
 pip3 install twine
 
+#clean up old versions
+rm -r .eggs
+rm -r dist
+rm -r neutronics_material_maker.egg-info/
+
+# increase version umber in setup.py
 bump
+
+# create distribution
 python3 setup.py sdist
+
+#upload to pypi
 twine upload dist/* --verbose
