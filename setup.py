84004ed7a67f6ae12e2b23065f320abdeeea4527
shimwell <shimwell@yahoo.com>
2020-05-21 17:15:16 +0100

    added data to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="neutronics_material_maker",
-    version="0.1235.24",
+    version="0.1235.25",
     summary='Package for making material cards for OpenMC',
     author="Jonathan Shimwell",
     author_email="jonathan.shimwell@ukaea.uk",
@@ -15,7 +15,7 @@ setuptools.setup(
     url="https://github.com/ukaea/neutronics_material_maker",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    package_data={'':['requirements.txt', 'README.md', 'LICENSE']},
+    package_data={'neutronics_material_maker':['requirements.txt', 'README.md', 'LICENSE', 'data/*.json']},
     # classifiers=[
     #     "Programming Language :: Python :: 3",
     #     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
