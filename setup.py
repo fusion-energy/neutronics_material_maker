ea5a0ccf6ff08b08cbe09eb774de203d8a386363
=shimwell <mail@jshimwell.com>
2020-07-21 12:54:56 +0100

    added python publish yml

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="neutronics_material_maker",
-    version="0.1235.35",
+    version="0.1235.37",
     summary='Package for making material cards for OpenMC',
     author="Jonathan Shimwell",
     author_email="jonathan.shimwell@ukaea.uk",
