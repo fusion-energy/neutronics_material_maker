f60c011741aeaf2a4fa16cdd72cf7b2e1f6ee91d
Ross Worrall <ross.worrall@ukaea.uk>
2020-09-24 08:59:39 +0100

    Change of variable from dp to decimal_places to conform to coding standards

--- neutronics_material_maker/utils.py
+++ neutronics_material_maker/utils.py
@@ -46,7 +46,7 @@ def make_serpent_material(mat):
     """Returns the material in a string compatable with Serpent II"""
 
     # decimal places to print
-    dp = 6
+    decimal_places = 6
 
     if mat.material_tag is None:
         name = mat.material_name
@@ -72,7 +72,7 @@ def make_serpent_material(mat):
             + isotope_to_zaid(isotope[0])
             + zaid_suffix
             + prefix
-            + f'{isotope[1]:{dp}e}'
+            + f'{isotope[1]:{decimal_places}e}'
         )
 
     return "\n".join(mat_card)
@@ -82,7 +82,7 @@ def make_mcnp_material(mat):
     """Returns the material in a string compatable with MCNP6"""
 
     # Number of decimal places to print
-    dp = 8
+    decimal_places = 8
 
     if mat.material_id is None:
         raise ValueError(
@@ -103,7 +103,7 @@ def make_mcnp_material(mat):
         "c     "
         + name
         + " density "
-        + f'{mat.openmc_material.get_mass_density():{dp}e}'
+        + f'{mat.openmc_material.get_mass_density():{decimal_places}e}'
         + " g/cm3"
     ]
     for i, isotope in enumerate(mat.openmc_material.nuclides):
@@ -119,7 +119,7 @@ def make_mcnp_material(mat):
             prefix = " -"
 
         rest = isotope_to_zaid(isotope[0]) + \
-            zaid_suffix + prefix + f'{isotope[1]:{dp}e}'
+            zaid_suffix + prefix + f'{isotope[1]:{decimal_places}e}'
 
         mat_card.append(start + rest)
 
