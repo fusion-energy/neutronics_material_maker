d56c8eaafd12bb9b4461ccf851e0f16c1785a03a
autopep8 <autopep8@users.noreply.github.com>
2021-02-24 13:59:54 +0000

    Automated autopep8 fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,12 +28,12 @@ version = ""
 # The full version, including alpha/beta/rc tags
 release = "1.0"
 
-# cool props doesn't support python 3.9 currently 
+# cool props doesn't support python 3.9 currently
 # https://github.com/CoolProp/CoolProp/issues/1981
 python:
-   version: 3.8
-   install:
-   - requirements: requirements.txt
+    version: 3.8
+    install:
+    - requirements: requirements.txt
 
 # -- General configuration ---------------------------------------------------
 
