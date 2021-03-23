7bcd27c6b4ebe00e07ecb3e7f6574a32cbf89f80
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2021-03-23 14:52:31 +0000

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="neutronics_material_maker",
-    version="0.2.0",
+    version="0.2.1",
     summary="Package for making material cards for neutronics codes",
     author="neutronics_material_maker development team",
     author_email="mail@jshimwell.com",
